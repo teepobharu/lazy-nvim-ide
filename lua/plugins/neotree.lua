6dcff2a0ebe9f26d1b515a09e1cf3d3e16493916
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-05-16 01:23:57 +0700

    feat: startify, keymap not work, telescope try

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,26 @@
+-- editor.lua in Lazy override
+
+return {
+
+  -- file explorer
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    -- https://github.com/LazyVim/LazyVim/blob/b601ade71c7f8feacf62a762d4e81cf99c055ea7/lua/lazyvim/plugins/editor.lua
+  opts = function(_, opts)
+      table.insert(opts.filesystem.window.mappings,
+        {
+          ["<Esc>"] = "clear_filter",
+          ["/"] = "none",
+          ["f"] = "fuzzy_finder",
+          ["F"] = "filter_on_submit",
+        })
+
+      table.insert(opts.filesystem.buffers.mappings,
+        {
+          ["x"] = "buffer_delete",
+        })
+
+      return opts;
+    end,
+  }
+
